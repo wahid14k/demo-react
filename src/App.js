d39d72db4c3ab2ac6593213507734e96a9b3ccba
wahid14k <wahid14k@gmail.com>
2021-11-17 15:27:08 +0600

    packeg ready

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react'
 import './App.css';
+import {
+  
+  Route,
+  Routes
+} from 'react-router-dom'
+
 import HomePage from './pages/homepages/homepage.component'
+
+
+const HatsPage =()=>(
+  <div>
+    <h1>Hats Page</h1>
+    <h2>fk offfff</h2>
+  </div>
+)
 
 function App() {
   return (
     <div>
-<HomePage/>  
+    <Routes>
+        <Route exact path='/' element={<HomePage/>}/>
+        <Route  path='/hats' element={<HatsPage/>}/>
+    </Routes>
     </div>
   );
 }
